237ef43a500304f0dd860eb653fd63cbb58296e0
kiruba0730-code <kk1273624@gmail.com>
2026-08-18 19:10:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,32 +2,40 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 const server = http.createServer((req, res) => {
-  let filePath = path.join(__dirname, "public", "index.html");
+    let filePath;
 
-  if (req.url !== "/" && req.url !== "/index.html") {
-    res.writeHead(404);
-    res.end("Not Found");
-    return;
-  }
+    if (req.url === "/" || req.url === "/index.html") {
+        filePath = path.join(__dirname, "public", "index.html");
+    } else {
+        res.writeHead(404, {
+            "Content-Type": "text/plain"
+        });
 
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      res.end("Server Error");
-      return;
+        res.end("Page Not Found");
+        return;
     }
 
-    res.writeHead(200, {
-      "Content-Type": "text/html"
-    });
+    fs.readFile(filePath, (error, data) => {
+        if (error) {
+            res.writeHead(500, {
+                "Content-Type": "text/plain"
+            });
 
-    res.end(data);
-  });
+            res.end("Internal Server Error");
+            return;
+        }
+
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+
+        res.end(data);
+    });
 });
 
 server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Calculator running on port ${PORT}`);
+    console.log(`Calculator application running on port ${PORT}`);
 });
